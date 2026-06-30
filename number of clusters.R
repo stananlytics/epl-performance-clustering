a900a77ce4fa226f00db7c5be22783ad5f7a15cc
Stanley Agbam <agbamtochukwu53@gmail.com>
2026-06-30 06:45:29 +0100

    Add files via upload

---
+++ number of clusters.R
@@ -0,0 +1,4 @@
+#objective 1: I.Classify professional football teams into distinct performance-based groups
+#using unsupervised machine learning techniques (clustering) based 
+#on match outcomes and goal-related indicators such as wins, draws, 
+#losses, goals scored, goals conceded, and goal difference. 
